3528b5281ffd1e4201be0cde14c6d51810a62e50
4orce <62334721+Pride5@users.noreply.github.com>
2023-01-19 18:16:19 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+//  direction: 'vertical', Что бы сделать верикальным сайт
+ // freeMode: true,
+  speed: 2400,
+  mousewheel:{
+    enabled: true,
+    sensitivity: 2.4
+  },
+  spaceBetween: 18,
+  parallax: true,
+})
